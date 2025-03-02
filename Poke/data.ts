93df90ced1c9b39f132755548e40b22045a26fe9
Tatiana <tarole.1002@gmail.com>
2025-03-01 21:32:33 -0400

    nestjs practica 2

---
+++ Poke/data.ts
@@ -0,0 +1,11 @@
+export interface Typescript {
+    count:    number;
+    next:     null;
+    previous: null;
+    results:  Result[];
+}
+
+export interface Result {
+    name: string;
+    url:  string;
+}
